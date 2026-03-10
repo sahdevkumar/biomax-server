c62ade78923de243e10d5d1f66e72e8c72dbd3f1
sahdevkumar <internet.00090@gmail.com>
2026-03-10 14:55:08 +0530

    Delete getrequest.js

--- getrequest.js
+++
@@ -1,9 +0,0 @@
-const { parseQuery } = require('./_helpers');
-
-module.exports = function handler(req, res) {
-  const { SN } = parseQuery(req.url);
-  console.log(`[getrequest] SN=${SN} polling for commands`);
-  // TODO: query a command_queue table and return pending commands here
-  res.setHeader('Content-Type', 'text/plain');
-  return res.status(200).send('OK');
-};
